0d85463177e27d2911da0279f8aca46d91141ebc
baanpangngoo-del <baanpang.ngoo@gmail.com>
2026-06-30 14:09:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ไฟล์ตั้งค่าส่วนกลางสำหรับเชื่อมต่อระบบหน้าบ้าน (Frontend)
-const LIFF_ID = "2010500776-6r1shNwL"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbyzPy7sklwURcJiqFDzlK4OEQ8616CJLuNyAjb8euQNzYJLXz_dj4UJM63w8-nI1L3r/exec";
+const LIFF_ID = "2010548670-letcjhpK"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbz3t5vRZt3NYZZjoygWe8ME8WOMtVwZ2YE1iIDZSvKZ0W6iLcBx9x566M3iE0f2IgFa/exec";
